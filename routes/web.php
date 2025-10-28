b2b7c7db55890e5e6c346804afdc60e5ad31c6e8
busayo524 <olowookerebusayo66@gmail.com>
2025-10-28 14:16:38 +0100

    installed breeze for login and register page

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,24 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/register',  [MovieController::class, 'register'
-])->name('show.register');
-Route::get('/login',  [MovieController::class, 'login'
-])->name('show.login');
 
-Route::get('/',  [MovieController::class, 'index'
-])->name('movies.index');
-Route::get('/movies', [MovieController::class, 'index'
-])->name('movies.index');
-Route::get('/movies/add', [MovieController::class, 'add'
-])->name('movies.add');
-Route::get('/movies/{movie}', [MovieController::class, 'show'
-])->name('movies.show');
-Route::post('/movies', [MovieController::class, 'store'
-])->name('movies.store');
-Route::delete('/movies/{movie}', [MovieController::class, 'destroy'
-])->name('movies.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [MovieController::class, 'index'])->name('movies.index');
+Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
+Route::get('/movies/add', [MovieController::class, 'add'])->name('movies.add');
+Route::post('/movies', [MovieController::class, 'store'])->name('movies.store');
+Route::delete('/movies/{movie}', [MovieController::class, 'destroy'])->name('movies.destroy');
+Route::get('/movies/{movie}', [MovieController::class, 'show'])->name('movies.show');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
